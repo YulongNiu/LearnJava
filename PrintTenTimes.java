6f8b0ca71e564dbb974d8a1a50788f9609586d1d
Yulong Niu <niuylscu@gmail.com>
2017-05-13 22:29:30 +0800

    first commit

---
+++ PrintTenTimes.java
@@ -0,0 +1,12 @@
+public class PrintTenTimes
+{
+    public static void main (String[] args)
+    {
+        for (int i = 0; i < 10; ++i)
+        {
+            System.out.print("Hello, World! \n");
+        }
+
+        System.out.println();
+    }
+}
