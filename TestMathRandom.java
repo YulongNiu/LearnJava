8663d90ab2f0c41540e008c424c57b2d1ffa1562
Yulong Niu <niuylscu@gmail.com>
2017-05-14 00:36:57 +0800

    test random

---
+++ TestMathRandom.java
@@ -0,0 +1,9 @@
+public class TestMathRandom
+{
+    public static void main (String[] args)
+    {
+        double r = Math.random();
+        System.out.print(r + "\n");
+        System.out.println(Math.sqrt(r));
+    }
+}
