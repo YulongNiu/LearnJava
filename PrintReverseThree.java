5591370bea1b5d806a682956c859cfa2a9a7562f
Yulong Niu <niuylscu@gmail.com>
2017-05-13 22:34:46 +0800

    use +

--- PrintReverseThree.java
+++ PrintReverseThree.java
@@ -2,12 +2,6 @@ public class PrintReverseThree
 {
     public static void main (String[] args)
     {
-        System.out.print("Hi ");
-        System.out.print(args[2]);
-        System.out.print(", ");
-        System.out.print(args[1]);
-        System.out.print(", and ");
-        System.out.print(args[0]);
-        System.out.println(". \n");
+        System.out.println("Hi " + args[2] +  ", " + args[1] + ", and " + args[0] + ". \n");
     }
 }
